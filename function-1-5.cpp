46f6779e90b1e8cabd884b4da50accbc38ed6fce
hgaedtke <haydn.gaedtke18@gmail.com>
2022-07-26 15:44:17 +0930

    testing

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
 int count_evens(int number) {
+    if (number < 1) {
+        return 0;
+    }   
+    
     int count = 0;
     for (int i = 1; i < number; i++) {
         if (i % 2 == 0) {
             count++;
         }
-    }
-
-    if (number < 1) {
-        return 0;
     }
 
     return count;
