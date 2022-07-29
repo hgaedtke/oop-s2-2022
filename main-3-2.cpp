fe9f37d74dd8164ef639bb76a92662b9b50afcbb
hgaedtke <haydn.gaedtke18@gmail.com>
2022-07-29 12:07:25 +0930

    Q3-2

--- main-3-2.cpp
+++ main-3-2.cpp
@@ -3,7 +3,7 @@
 extern int median_array(int array[], int n);
 
 int main() {
-    int array[6] = {3, 5, 2, 1, 4, 6};
-    std::cout << "The median of array is: " << median_array(array, 6) << std::endl;
+    int array[5] = {3, 5, 2, 1, 4};
+    std::cout << "The median of array is: " << median_array(array, 5) << std::endl;
     return 0;
 }
