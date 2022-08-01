f8e3129582fa3d1340c99a848668b7d91d4e7c8b
hgaedtke <haydn.gaedtke18@gmail.com>
2022-08-01 13:48:57 +0930

    transform file

---
+++ transform.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+void convert(unsigned int value) {
+  int array[32] = {0};
+
+  for (int i = 31; i > -1 && value > 0; i--) {
+    array[i] = value % 2;
+    value = value / 2;
+  }
+
+  for (int i = 0; i < 32; i++) {
+    std::cout << array[i];
+  }
+
+  std::cout << std::endl;
+}
+
+int main() {
+    unsigned int value = 18;
+    convert(value);
+}
