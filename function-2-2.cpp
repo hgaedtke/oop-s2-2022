701347d964e19ef4689dbde1d72d9b5bbe4cc343
hgaedtke <haydn.gaedtke18@gmail.com>
2022-08-04 23:05:23 +0930

    Q2-2

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,7 +1,10 @@
+#include <iostream>
+#include <math.h>
+
 int binary_to_int(int binary_digits[], int number_of_digits) {
     int number = 0;
     for (int i = 0; i < number_of_digits; i++) {
-        number = number + (binary_digits[i] * (2 ^ number_of_digits - i - 1));
+        number += binary_digits[i] * pow(2, number_of_digits - i - 1);
     }
     
     return number;
