71c500fb62c51c788f0aa822fa2af31d8df7f3a9
hgaedtke <haydn.gaedtke18@gmail.com>
2022-08-04 21:51:58 +0930

    Q1-5

---
+++ function-1-5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void print_summed(int array1[3][3], int array2[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            array1[i][j] += array2[i][j];
+            if (j < 2) {
+                std::cout << array1[i][j] << " ";
+            }
+            if (j >= 2) {
+                std::cout << array1[i][j] << std::endl;
+            }
+        }
+    }
+}
