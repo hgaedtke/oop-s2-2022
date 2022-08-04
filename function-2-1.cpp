693da80f956aee43626ecf3b31e3cfddabf6fe21
hgaedtke <haydn.gaedtke18@gmail.com>
2022-08-04 22:32:51 +0930

    Q2-1

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -6,13 +6,18 @@ void print_binary_str(std::string decimal_number) {
   int i_number = std::stoi(decimal_number, &size);
 
   int array[32] = {0};
+  int index = 0;
 
   for (int i = 31; i > -1 && i_number > 0; i--) {
     array[i] = i_number % 2;
     i_number = i_number / 2;
   }
 
-  for (int i = 0; i < 32; i++) {
+    for (int i = 0; i < 31 && array[i] == 0; i++) {
+        index += 1;
+    }
+
+  for (int i = index; i < 32; i++) {
     std::cout << array[i];
   }
 
