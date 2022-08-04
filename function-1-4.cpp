4136ce1cd36c61d639c50d6f3b446497be107675
hgaedtke <haydn.gaedtke18@gmail.com>
2022-08-04 21:39:55 +0930

    Q1-4

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -5,10 +5,10 @@ void print_scaled(int array[3][3], int scale) {
         for (int j = 0; j < 3; j++) {
             array[i][j] = scale * array[i][j];
             if (j < scale - 1) {
-                std::cout << array[i][j] * scale << " ";
+                std::cout << array[i][j] << " ";
             }
             if (j >= scale - 1) {
-                std::cout << array[i][j] * scale << std::endl;
+                std::cout << array[i][j] << std::endl;
             }
         }
     }
