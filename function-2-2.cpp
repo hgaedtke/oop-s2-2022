b80d6e50118ba465f5373a735a2c5522f12639ab
hgaedtke <haydn.gaedtke18@gmail.com>
2022-08-05 12:43:42 +0930

    Initial commit

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -6,6 +6,5 @@ int binary_to_int(int binary_digits[], int number_of_digits) {
     for (int i = 0; i < number_of_digits; i++) {
         number += binary_digits[i] * pow(2, number_of_digits - i - 1);
     }
-    
     return number;
 }
