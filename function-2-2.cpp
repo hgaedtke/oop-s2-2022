e3de4339722250c94e599b6204f70e79cbd34c68
hgaedtke <haydn.gaedtke18@gmail.com>
2022-08-12 14:28:20 +0930

    Q2-2

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -4,7 +4,7 @@
 int bin_to_int(int binary_digits[], int number_of_digits) {
     int number = 0;
     for (int i = 0; i < number_of_digits; i++) {
-        
+        number += binary_digits[i] * pow(2, number_of_digits - i - 1);
     }
     return number;
 }
