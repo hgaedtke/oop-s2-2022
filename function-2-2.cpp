3c78a2e9e5022341b6ed8a301739346210a69df2
hgaedtke <haydn.gaedtke18@gmail.com>
2022-08-12 14:26:30 +0930

    Initial commit

---
+++ function-2-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+
+int bin_to_int(int binary_digits[], int number_of_digits) {
+    int number = 0;
+    for (int i = 0; i < number_of_digits; i++) {
+        
+    }
+    return number;
+}
+
+
+
+
+// 1
+// 0
+// 4
+// 0
+// 16
---
+++ main-2-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+extern int bin_to_int(int binary_digits[], int number_of_digits);
+
+int main() {
+    int binary_digits[5] = {1, 0, 1, 0, 1};
+    int number_of_digits = 5;
+    std::cout << bin_to_int(binary_digits, number_of_digits);
+    return 0;
+}
