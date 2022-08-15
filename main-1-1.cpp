54569f7d2b34f2528c33560ac82a557ea08cd4d1
hgaedtke <haydn.gaedtke18@gmail.com>
2022-08-15 14:16:11 +0930

    Q1-1

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 
-extern void printer(int array[10][10]);
+extern int size_of_variable_star_t();
 
 int main() {
-    int array[10][10] = {0};
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            array[i][j] = i * 10 + j;
-        }
-    }
-    printer(array);
+    std::cout << size_of_variable_star_t() << std::endl;
+    return 0;
 }
