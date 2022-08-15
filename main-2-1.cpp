f16a1f020af1f8df1827f08371f12328fef384b8
hgaedtke <haydn.gaedtke18@gmail.com>
2022-08-15 14:41:56 +0930

    Initial commit

---
+++ main-2-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+extern float add_op(float left, float right);
+extern float subtract_op(float left, float right);
+extern float arithmetic_ops(float left, float right, std::string op);
+
+int main() {
+    return 0;
+}
