ba867b463f319c22d23f25e931ca325bd1ca30cd
hgaedtke <haydn.gaedtke18@gmail.com>
2022-08-15 14:28:10 +0930

    Q1-5

---
+++ function-1-5.cpp
@@ -0,0 +1,5 @@
+int size_of_array_arr() {
+    int *arr = new int[3];
+    int size = sizeof(arr);
+    return size;
+}
---
+++ main-1-5.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+extern int size_of_array_arr();
+
+int main() {
+    std::cout << size_of_array_arr() << std::endl;
+    return 0;
+}
