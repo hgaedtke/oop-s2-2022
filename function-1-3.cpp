e50b543520a0c9bfd2d7d075c12ae0c92c123796
hgaedtke <haydn.gaedtke18@gmail.com>
2022-08-19 15:19:26 +0930

    Q1-3

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
 void copy_integers(int old_array[], int new_array[], int length) {
-    *new_array = *old_array;
+    for (int i = 0; i < length; i++) {
+        *new_array++ = *old_array++;
+    }
 }
