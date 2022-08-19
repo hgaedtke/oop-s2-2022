6b577e8e7381a39d19724eeeb36506ec05471172
hgaedtke <haydn.gaedtke18@gmail.com>
2022-08-19 15:31:25 +0930

    Q1-4

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
 void copy_doubles(double *old_array, double *new_array, int length) {
-    for (int i = 0; i < length; i++) {
-        new_array = old_array;
-    }    
+    new_array = old_array;
 }
