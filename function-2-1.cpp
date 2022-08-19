8a1078751f1d449fa6d74b58688759c0e147b29b
hgaedtke <haydn.gaedtke18@gmail.com>
2022-08-19 15:58:44 +0930

    Q2-1

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -2,8 +2,8 @@
 
 void multiples_of_seven(int *nums, int length) {
     for (int i = 0; i < length; i++) {
-        if (*(++nums) % 7 == 0) {
-            std::cout << *(++nums) << std::endl;
+        if (nums[i] % 7 == 0) {
+            std::cout << nums[i] << std::endl;
         }
     }
 }
