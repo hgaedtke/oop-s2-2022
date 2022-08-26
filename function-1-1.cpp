5f0f23db272e156fd2b79fdaffa6b1592dc99860
hgaedtke <haydn.gaedtke18@gmail.com>
2022-08-26 15:23:47 +0930

    Q1-1

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,3 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
 int *array_pad(int *vals, int len) {
-    
+    int length = len + 2;
+    int pv[7] = {0};
+    pv[0] = vals[0];
+    for (int i = 0; i < len; i++) {
+        pv[i + 1] = vals[i];
+    }
+    pv[6] = vals[4];
+    return 0;
 }
