db37163bcd6427fd3b6f2b4b5141ac5a34ad539c
hgaedtke <haydn.gaedtke18@gmail.com>
2022-08-26 15:32:13 +0930

    Q1-1

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -4,11 +4,8 @@ using namespace std;
 
 int *array_pad(int *vals, int len) {
     int *pv = new int[len+2];
-    for (int i = 0; i < len; i++) {
-        if (i == 0 || i == len) {
-            *pv++ = vals[i];
-        }
-        *pv++ = vals[i];
+    for (int i = 0; i < len + 2; i++) {
+        *pv++ = *vals++;
     }
     return pv;
 }
