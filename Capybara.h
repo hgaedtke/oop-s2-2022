fb70692cb13db6428397beeafab537b16e566ab1
hgaedtke <haydn.gaedtke18@gmail.com>
2022-09-02 20:28:57 +0930

    Q1-1

---
+++ Capybara.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Capybara{
+    Capybara();
+    void setName(string capyName);
+    string getName();
+    void setAge(int capyAge);
+    int getAge();
+};
