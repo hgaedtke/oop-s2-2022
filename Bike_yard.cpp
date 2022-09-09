f35d47a29e92ade123cdc831a95db997894e2729
hgaedtke <haydn.gaedtke18@gmail.com>
2022-09-09 14:52:01 +0930

    Q1-2

---
+++ Bike_yard.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include "Bike_yard.h"
+#include "Bike.h"
+
+Bike_yard::Bike_yard(){
+    capacity = 0;
+}
+
+Bike_yard::Bike_yard(int capacity){
+
+}
+
+int Bike_yard::get_total_stock_count(){
+    
+}
+
+int Bike_yard::get_stock_quantity(int code){
+
+}
+
+Bike Bike_yard::*get_current_stock_list(){
+
+}
+
+bool Bike_yard::add_stock(Bike b){
+
+}
+
+Bike_yard::~Bike_yard(){
+
+}
---
+++ Bike_yard.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "Bike.h"
+
+using namespace std;
+
+class Bike_yard{
+    public:
+    int capacity;
+    Bike_yard();
+    Bike_yard(int capacity);
+    int get_total_stock_count();
+    int get_stock_quantity(int code);
+    Bike *get_current_stock_list();
+    bool add_stock(Bike b);
+    ~Bike_yard();
+};
