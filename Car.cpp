399b2808d6aa1d6e17d511dae016d3d0e332d154
hgaedtke <haydn.gaedtke18@gmail.com>
2022-10-28 14:27:47 +1030

    Initial

--- Car.cpp
+++ Car.cpp
@@ -2,3 +2,27 @@
 #include <iostream>
 
 using namespace std;
+
+int Car::get_price() {
+    return price;
+}
+
+int Car::set_price() {
+    cin >> price;
+}
+
+int Car::get_emissions() {
+    return emissions;
+}
+
+int Car::set_emissions() {
+    cin >> emissions;
+}
+
+int Car::get_vinNumber() {
+    return vinNumber;
+}
+
+int Car::set_vinNumber() {
+    cin >> vinNumber;
+}
--- Car.h
+++ Car.h
@@ -10,5 +10,13 @@ Car(int price);
 int price;
 int vinNumber;
 int emissions;
+int cost;
 
+protected:
+int get_price();
+int get_emissions();
+int get_vinNumber();
+int set_price();
+int set_emissions();
+int set_vinNumber();
 };
