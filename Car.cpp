07c5c196a82e0a5de787f32cbd49aa7a22cd6026
hgaedtke <haydn.gaedtke18@gmail.com>
2022-10-28 14:30:52 +1030

    Initial

--- Car.cpp
+++ Car.cpp
@@ -9,6 +9,7 @@ int Car::get_price() {
 
 int Car::set_price() {
     cin >> price;
+    return price;
 }
 
 int Car::get_emissions() {
@@ -17,6 +18,7 @@ int Car::get_emissions() {
 
 int Car::set_emissions() {
     cin >> emissions;
+    return emissions;
 }
 
 int Car::get_vinNumber() {
@@ -25,4 +27,5 @@ int Car::get_vinNumber() {
 
 int Car::set_vinNumber() {
     cin >> vinNumber;
+    return emissions;
 }
