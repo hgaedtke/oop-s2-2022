9949117c01b9c679d503ef24a00424beb3bb7859
hgaedtke <haydn.gaedtke18@gmail.com>
2022-10-28 15:47:10 +1030

    ini

--- Tesla.cpp
+++ Tesla.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 void Tesla::drive(int kms){
-    batteryPercentage = 100 - (float(kms) * 0.2);
+    batteryPercentage = 100 - (kms * 0.2);
     if (batteryPercentage > 0) {
         emissions += 74;
     }
@@ -14,6 +14,7 @@ void Tesla::drive(int kms){
 Tesla::Tesla(char model, int price){
     Tesla::model = model;
     Car::price = price;
+    Car::emissions = 0;
     Tesla::batteryPercentage = 100;
     int index = 0;
     vinNumber = 1000001 + index;
