348910b32fd0f038053c2de313fc303f3cfc56e2
hgaedtke <haydn.gaedtke18@gmail.com>
2022-10-28 14:44:18 +1030

    Initial

--- Car.cpp
+++ Car.cpp
@@ -3,11 +3,18 @@
 
 using namespace std;
 
+Car::Car(int price) {
+    Car::price = price;
+    Car::emissions = 0;
+    Car::vinNumber;
+}
+
 int Car::get_price() {
     return price;
 }
 
-int Car::set_price(Car * price) {
+int Car::set_price(int price) {
+    Car::price = price;
     return Car::price;
 }
 
@@ -15,7 +22,8 @@ int Car::get_emissions() {
     return emissions;
 }
 
-int Car::set_emissions(Car * emissions) {
+int Car::set_emissions(int emissions) {
+    Car::emissions = emissions;
     return Car::emissions;
 }
 
@@ -23,6 +31,6 @@ int Car::get_vinNumber() {
     return vinNumber;
 }
 
-int Car::set_vinNumber(Car * vinNumber) {
+int Car::set_vinNumber(int vinNumber) {
     return Car::vinNumber;
 }
