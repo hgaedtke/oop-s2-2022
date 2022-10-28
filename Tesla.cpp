30a87cfafc4e80611b2014b52a7200a427171506
hgaedtke <haydn.gaedtke18@gmail.com>
2022-10-28 15:42:35 +1030

    ini

--- Tesla.cpp
+++ Tesla.cpp
@@ -1,11 +1,8 @@
 #include "Tesla.h"
 #include <iostream>
 
-int vinNumber = 1000001;
-
 void Tesla::drive(int kms){
     batteryPercentage = 100 - (kms * 0.2);
-    emissions = 0;
     if (batteryPercentage > 0) {
         emissions += 74;
     }
@@ -18,7 +15,7 @@ Tesla::Tesla(char model, int price){
     Tesla::model = model;
     Car::price = price;
     Tesla::batteryPercentage = 100;
-    vinNumber += 1000001;
+    vinNumber = 1000001;
 }
 
 void Tesla::chargeBattery(int mins){
