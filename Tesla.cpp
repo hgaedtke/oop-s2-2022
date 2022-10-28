61a34e9a3825fac92c7ef4f74de117bc5c635f99
hgaedtke <haydn.gaedtke18@gmail.com>
2022-10-28 15:31:20 +1030

    ini

--- Tesla.cpp
+++ Tesla.cpp
@@ -6,19 +6,21 @@ void Tesla::drive(int kms){
     if (batteryPercentage > 0) {
         emissions += 74;
     }
+    if (batteryPercentage < 0) {
+        batteryPercentage = 0;
+    }
 }
 
 Tesla::Tesla(char model, int price){
     Tesla::model = model;
     Car::price = price;
     Tesla::batteryPercentage = 100;
+    vinNumber += 100001;
 }
 
 void Tesla::chargeBattery(int mins){
     batteryPercentage += mins * 0.5;
 }
-
-int VinNumber = 1000001;
 
 char Tesla::get_model() {
     return model;
